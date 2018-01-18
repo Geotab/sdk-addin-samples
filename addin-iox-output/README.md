93f98aa802848f7b56e4ebed9ffa8e3758752a51
Beefo <beefo@hotmail.com>
2018-01-18 14:02:43 -0500

    Add IOX output sample

---
+++ addin-iox-output/README.md
@@ -0,0 +1,31 @@
+# addin-iox-output
+This example demonstrates how to send a message to a device, turning the IOX on or off.
+
+## Getting Started
+
+This add-in was developed using [generator-addin](https://github.com/Geotab/generator-addin) to allow local developmented and testing.
+
+* Install [nodejs](https://nodejs.org/en/) latest LTS
+* Install dependencies: npm install -g yo gulp-cli bower
+* Clone the samples repository 'git clone https://github.com/Geotab/sdk-addin-samples.git sdk-addin-samples
+* Naviagte to the working directory `cd sdk-addin-samples/addin-iox-output`
+* Run the sample `> gulp serve`
+
+## Installation
+Add the configuration below to the to the system setting -> add-ins section of the MyGeotab database
+
+```json
+{
+  "name": "IOX Output",
+  "supportEmail": "support@geotab.com",
+  "version": "1.0.0",
+  "items": [{
+    "url": "https://cdn.rawgit.com/Geotab/sdk-addin-samples/master/addin-iox-output/dist/ioxOutput.html",
+    "path": "AdministrationLink/",
+    "menuName": {
+      "en": "IOX Output"
+    },
+    "icon": "https://cdn.rawgit.com/Geotab/sdk-addin-samples/master/addin-iox-output/dist/images/icon.svg"
+  }]
+}
+```
---
+++ addin-iox-output/app/scripts/main.js
@@ -0,0 +1,130 @@
+/**
+ * @returns {{initialize: Function, focus: Function, blur: Function}}
+ */
+geotab.addin.ioxOutput = () => {
+    'use strict';
+
+    let api;
+
+    let elVehicleSelect;
+    let elSendButton;
+    let elSendHistory;
+    let elError;
+    let elStateSelect;
+
+    let errorHandler = message => {
+        elError.innerHTML = message;
+    };
+
+    let pollForResult = divId => {
+        setTimeout(() => {
+            api.call('Get', {
+                typeName: 'TextMessage',
+                search: {
+                    id: divId.split('-')[0]
+                }
+            }, testMessages => {
+                if (testMessages[0].delivered) {
+                    let element = document.getElementById(divId);
+                    element.innerHTML += ', Delivered: ' + new Date(testMessages[0].delivered);
+                } else {
+                    pollForResult(divId);
+                }
+            }, errorHandler);
+        }, 1000);
+    };
+
+    let appendAndPoll = textMessageId => {
+        let divId = textMessageId + '-' + Date.now(),
+            headerElement = document.createElement('h4'),
+            textElement = document.createTextNode('Sent: ' + new Date());
+
+        headerElement.setAttribute('id', divId);
+        headerElement.appendChild(textElement);
+        elSendHistory.appendChild(headerElement);
+
+        pollForResult(divId);
+    };
+
+    let sendTextMessage = () => {
+        let deviceId = elVehicleSelect.value,
+            state = elStateSelect.options[elStateSelect.selectedIndex].text;
+
+        api.call('Add', {
+            typeName: 'TextMessage',
+            entity: {
+                device: {
+                    id: deviceId
+                },
+                messageContent: {
+                    isRelayOn: state === 'On',
+                    contentType: 'IoxOutput'
+                },
+                isDirectionToVehicle: true
+            }
+        }, appendAndPoll, errorHandler);
+    };
+
+    let sortNameEntities = (a, b) => {
+        a = a.name.toLowerCase();
+        b = b.name.toLowerCase();
+        if (a === b) {
+            return 0;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return -1;
+        }
+    };
+
+    return {
+        initialize(geotabApi, pageState, initializeCallback) {
+            api = geotabApi;
+
+            elVehicleSelect = document.getElementById('ioxoutput-vehicles');
+            elSendButton = document.getElementById('ioxoutput-send');
+            elSendHistory = document.getElementById('ioxoutput-history');
+            elError = document.getElementById('ioxoutput-error');
+            elStateSelect = document.getElementById('ioxoutput-state');
+
+            elVehicleSelect.addEventListener('change', () => {
+                elSendButton.disabled = false;
+            });
+
+            elSendButton.addEventListener('click', sendTextMessage);
+
+            initializeCallback();
+        },
+        focus(geotabApi) {
+            api = geotabApi;
+
+            api.call('Get', {
+                typeName: 'Device',
+                resultsLimit: 1000,
+                search: {
+                    fromDate: new Date().toISOString()
+                }
+            }, devices => {
+                if (!devices || devices.length < 1) {
+                    return;
+                }
+
+                devices.sort(sortNameEntities);
+
+                elVehicleSelect.innerHTML = '';
+
+                for (let i = 0; i < devices.length; i++) {
+                    let option = new Option();
+                    option.text = devices[i].name;
+                    option.value = devices[i].id;
+                    elVehicleSelect.add(option);
+                }
+
+                elSendButton.disabled = false;
+            }, errorHandler);
+        },
+        blur() {
+            elSendButton.disabled = true;
+        }
+    };
+};
