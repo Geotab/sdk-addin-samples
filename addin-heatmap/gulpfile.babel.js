160597418545c738ab17fc8af5448d6080d3070d
Steve Hansen <stevehansen@geotab.com>
2019-10-29 12:26:10 -0400

    Fix autoprefixer warning

--- addin-heatmap/gulpfile.babel.js
+++ addin-heatmap/gulpfile.babel.js
@@ -15,9 +15,7 @@ const reload = browserSync.reload;
 gulp.task('styles', () => {
   return gulp.src('app/styles/*.css')
     .pipe($.sourcemaps.init())
-    .pipe($.autoprefixer({
-      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
-    }))
+    .pipe($.autoprefixer())
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(reload({
